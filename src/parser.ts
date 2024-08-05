ccc94974c826512c52aba753d470bafdc16813db
Igor Kowalski <igor.kowalski@qiagen.com>
2024-08-05 17:11:04 +0200

    2.1.4

--- src/parser.ts
+++ src/parser.ts
@@ -1,3 +1,4 @@
+/* eslint-disable import/no-default-export */
 interface ParserReturn {
     ast: {
         type: string;
@@ -23,27 +24,37 @@ interface ParserReturn {
     visitorKeys: null;
 }
 
-exports.parseForESLint = (): ParserReturn => ({
-    ast: {
-        type: "Program",
-        start: 0,
-        end: 0,
-        loc: {
-            start: {
-                line: 0,
-                column: 0,
+interface ParserProps {
+    parseForESLint: () => ParserReturn;
+}
+
+const parser: ParserProps = {
+    parseForESLint: () => ({
+        ast: {
+            type: "Program",
+            start: 0,
+            end: 0,
+            loc: {
+                start: {
+                    line: 0,
+                    column: 0,
+                },
+                end: {
+                    line: 0,
+                    column: 0,
+                },
             },
-            end: {
-                line: 0,
-                column: 0,
-            },
+            tokens: [],
+            comments: [],
+            range: [0, 0],
+            sourceType: "module",
+            body: [],
         },
-        tokens: [],
-        comments: [],
-        range: [0, 0],
-        sourceType: "module",
-        body: [],
-    },
-    scopeManager: null,
-    visitorKeys: null,
-});
+        scopeManager: null,
+        visitorKeys: null,
+    }),
+};
+
+// ts-prune-ignore-next
+export default parser;
+module.exports = parser;
