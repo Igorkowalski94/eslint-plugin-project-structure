11df61fa1e30ee55487665d3c1cfb31945a55731
Igor Kowalski <igorkowalski94@gmail.com>
2024-12-04 01:13:40 +0100

    3.10.7

--- src/rules/folderStructure/helpers/validateFolderStructure/helpers/validatePath/helpers/getChildren/helpers/sortChildrenByNameType.test.ts
+++ src/rules/folderStructure/helpers/validateFolderStructure/helpers/validatePath/helpers/getChildren/helpers/sortChildrenByNameType.test.ts
@@ -20,29 +20,32 @@ describe("sortChildrenByNameType", () => {
     { name: "use*", children: [] },
     { name: "{camelCase}.ts" },
     { name: "FixedName3.ts" },
+    { name: "custom-name1", children: [] },
+    { name: "custom_name2", children: [] },
+    { name: "{kebab-case}", children: [] },
+    { name: "{snake_case}", children: [] },
   ];
 
   const childrenSorted: Rule[] = [
     { name: "FixedName3.ts" },
     { name: "FixedName2.ts" },
     { name: "FixedName1.ts" },
-
     { name: "{camelCase}.ts" },
     { name: "{FolderName}.ts" },
-
     { name: "use*.ts" },
     { name: "*_{camelCase}.ts" },
     { name: "use*.ts" },
     { name: "*_{FolderName}.ts" },
-
     { name: "*" },
     { name: "*" },
-
+    { name: "custom_name2", children: [] },
+    { name: "custom-name1", children: [] },
+    { name: "{snake_case}", children: [] },
+    { name: "{kebab-case}", children: [] },
     { name: "use*", children: [] },
     { name: "use*", children: [] },
     { name: "*", children: [] },
     { name: "*", children: [] },
-
     { ruleId: "ruleId" },
     { ruleId: "ruleId" },
   ];
