952725c3bbc19df3016556efd5de4c9ca6c9b4a7
Artem Zhivaev <artem.zhivaev@mail.ru>
2025-01-14 11:00:32 +0300

    Support imports with query params

--- src/rules/independentModules/helpers/validateImport/helpers/validateAll/validateAll.ts
+++ src/rules/independentModules/helpers/validateImport/helpers/validateAll/validateAll.ts
@@ -5,6 +5,7 @@ import { checkImportPath } from "rules/independentModules/helpers/validateImport
 import { convertImportPathToNonRelative } from "rules/independentModules/helpers/validateImport/helpers/validateAll/helpers/convertImportPathToNonRelative";
 import { getImportPaths } from "rules/independentModules/helpers/validateImport/helpers/validateAll/helpers/getImportPaths";
 import { removeProjectRootFromPath } from "rules/independentModules/helpers/validateImport/helpers/validateAll/helpers/removeProjectRootFromPath";
+import { removeQueryParamsFromPath } from "rules/independentModules/helpers/validateImport/helpers/validateAll/helpers/removeQueryParamsFromPath";
 import { IndependentModulesConfig } from "rules/independentModules/independentModules.types";
 
 interface ValidateAllProps {
@@ -46,8 +47,12 @@ export const validateAll = ({
       projectRootWithBaseUrl,
     });
 
-    const importPathWithExtension = addExtensionToImportPath({
+    const importPathWithoutQueryParams = removeQueryParamsFromPath({
       importPath: importPathNonRelative,
+    });
+
+    const importPathWithExtension = addExtensionToImportPath({
+      importPath: importPathWithoutQueryParams,
       projectRootWithBaseUrl,
       extensions,
       projectRoot,
